0916b4df8d179739cbab6e7e9d29ff5d7a24c0b2
Kevin Xu <kevinxu820@gmail.com>
2020-02-07 21:33:22 -0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+data = pd.read_csv('abcnews-date-text.csv', error_bad_lines=False);
+data_text = data[['headline_text']]
+data_text['index'] = data_text.index
+documents = data_text
+
+print(len(documents))
+print(documents[:5])
+
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+from nltk import PorterStemmer
+import numpy as np
+np.random.seed(2018)
+import nltk
+nltk.download('wordnet')
+
+# Data Preprocessing
+
+def lemmatize_stemming(text):
+    return PorterStemmer().stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
+
+doc_sample = documents[documents['index'] == 4310].values[0][0]
+print('original document: ')
+words = []
+for word in doc_sample.split(' '):
+    words.append(word)
+print(words)
+print('\n\n tokenized and lemmatized document: ')
+print(preprocess(doc_sample))
+
+processed_docs = documents['headline_text'].map(preprocess)
+processed_docs[:10]
+
+
+# Bag of Words
+
+dictionary = gensim.corpora.Dictionary(processed_docs)
+count = 0
+for k, v in dictionary.iteritems():
+    print(k, v)
+    count += 1
+    if count > 10:
+        break
+
+dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+
+bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+bow_corpus[4310]
+
+bow_doc_4310 = bow_corpus[4310]
+for i in range(len(bow_doc_4310)):
+    print("Word {} (\"{}\") appears {} time.".format(bow_doc_4310[i][0], 
+                                               dictionary[bow_doc_4310[i][0]], 
+bow_doc_4310[i][1]))
+    
+
+#TF-IDF
+from gensim import corpora, models
+tfidf = models.TfidfModel(bow_corpus)
+corpus_tfidf = tfidf[bow_corpus]
+from pprint import pprint
+for doc in corpus_tfidf:
+    pprint(doc)
+    break
+
+
+# Running using Bag of Words
+lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=2)
+
+for idx, topic in lda_model.print_topics(-1):
+    print('Topic: {} \nWords: {}'.format(idx, topic))
